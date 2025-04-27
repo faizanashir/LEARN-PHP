5d01c02e49fc9baa300436cacb02b86c921bf57c
MUHAMMAD FAIZAN ASHIR <ashirfaizan57@gmail.com>
2025-04-28 01:40:18 +0500

    Create Class5.php
    This section is commint

---
+++ Class5.php
@@ -0,0 +1,9 @@
+<?php
+// Learn Comments.
+//$x ="Faizan";// This is single line comment.
+/*$x ="Faizan";
+$x ="Faizan";
+$x ="Faizan";
+$x ="Faizan";*/ // This is  multiline comment.
+echo $x;
+?>
