79f38fb6888a745bf478336e8848a9a1a80e87f0
MUHAMMAD FAIZAN ASHIR <ashirfaizan57@gmail.com>
2025-04-28 02:20:37 +0500

    Create Class7.php

---
+++ Class7.php
@@ -0,0 +1,28 @@
+<?php
+// Learn Arithmetic Operators
+
+//$a = 20.55;
+$a = 20;
+$b = 5;
+  
+// Ye sab Operators ha.
+
+//$c = $a + $b;  // Arithmetic Operator.
+
+//$c = $a - $b; // Subtraction Operator.
+
+//$c = $a * $b; // Multiplication Operator.
+
+//$c = $a / $b; // Division Operator.
+
+//$c = $a % $b; // Modulas Operator.
+
+//$c = $a ** $b; // Exponentation Operator.
+
+$a++;
+$a++; // Increment Operator.
+
+/*$a--;
+$a--;*/ // Decrement Operator.
+echo $a;
+?>
