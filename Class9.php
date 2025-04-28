6cf11747c81cbeeb2c9a1936c494ae6a6dc6b4d4
MUHAMMAD FAIZAN ASHIR <ashirfaizan57@gmail.com>
2025-04-28 15:39:50 +0500

    Create Class9.php

---
+++ Class9.php
@@ -0,0 +1,17 @@
+<?php
+// LEARN IF STATEMENT.
+
+$a = 30;
+//$b = 30;
+$b = "30";
+//if($a < $b)
+
+//if($a == $b)// This sign Means A and B value is same.
+
+if($a === $b) // This value is False.
+{
+   echo "A is smaller<br>"; 
+}
+
+echo "Here is other statement";
+?>
