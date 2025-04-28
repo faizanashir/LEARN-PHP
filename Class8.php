a0f49c54dbb19cc6da798aa7df10d25fd988f1dc
MUHAMMAD FAIZAN ASHIR <ashirfaizan57@gmail.com>
2025-04-28 15:24:32 +0500

    Create Class8.php

---
+++ Class8.php
@@ -0,0 +1,34 @@
+<?php
+// Learn Comparison Operators.
+
+$a = 20;
+$b = 10;
+
+//$b = 30;
+
+//echo $a == $b;
+
+//echo $a === $b;
+
+//echo $a != $b;
+
+//echo $a !== $b;
+
+//echo $a <> $b; // Ye not equal sign ki second type ha.
+
+//echo $a > $b;
+
+//echo $a < $b;
+
+//echo $a <= $b;
+
+//echo $a >= $b;
+
+echo $a <=> $b; // Ye 3 values return karta ha.
+
+// A value B value se less hogi (-1) Return kare ga.
+
+// A value B value ke barabar hogi  (0) Return kare ga.
+
+// A value B value Se greater ho (1) Return kare ga.
+?>
